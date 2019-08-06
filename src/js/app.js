16148c60d1b6f9705af8f0893b4f06a855f46f42
Harry Cresswell <studio@harrycresswell.com>
2019-08-06 09:20:54 +0200

    update(components): removed mobileMenu and hamburger

--- src/js/app.js
+++ src/js/app.js
@@ -1,17 +1,1 @@
-// MobileMenu and Hamburger
-$(document).ready(function(){
-    /* when .Hamburger is clicked */
-    $('.Hamburger').click(function(){
-      /* add .is-active to animate */
-      $(this).toggleClass('is-active');
-      /* add .is-visible to show nav */
-      $('.MobileMenu').toggleClass('is-visible');
-      /* when Nav link is clicked */
-      $('.Header-link').click(function(){
-        /* hide nav */
-        $('.MobileMenu').removeClass('is-visible');
-        /* and remove active class from Hamburger */
-        $('.MobileMenu').removeClass('is-active');
-      });
-    });
-  });
+// JS goes here
